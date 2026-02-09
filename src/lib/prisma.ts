2618e8ea419860c2aac38af5bf6c8e201fffc69b
Matheus <MatheusMLima@users.noreply.github.com>
2026-02-09 18:53:11 -0300

    Ajuste import Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/generated/prisma/client"
+import { PrismaClient } from "@/generated/prisma"
 import { withAccelerate } from "@prisma/extension-accelerate"
 import { fieldEncryptionExtension  } from 'prisma-field-encryption'
  
@@ -6,7 +6,7 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
  
 export const prisma =
   globalForPrisma.prisma || new PrismaClient().$extends(fieldEncryptionExtension({
-    encryptionKey: process.env.PRISMA_FIELD_ENCRYPTION_KEY,
+    encryptionKey: process.env.PRISMA_FIELD_ENCRYPTION_KEY!,
   }))
  
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
